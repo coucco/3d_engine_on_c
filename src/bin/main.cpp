635015ce91b3bcfcbbf1a02d488cddd19435cb68
coucco <timurshokarov1001@gmail.com>
2024-04-09 14:28:29 +0300

    исправлена функция отрисовки треугольников

--- src/bin/main.cpp
+++ src/bin/main.cpp
@@ -21,6 +21,12 @@ struct Vertex{
     long double z;
 };
 
+struct Color{
+    Uint8 r;
+    Uint8 g;
+    Uint8 b;
+};
+
 int width = 1080;
 int height = 1080;
 
@@ -59,45 +65,73 @@ void draw_line(SDL_Renderer* render, int x1, int y1, int x2, int y2) {
     }
 }
 
-void triangle(SDL_Renderer* render, Vec2i a, Vec2i b, Vec2i c) {
+void triangle(SDL_Renderer* render, Vec2i a, Vec2i b, Vec2i c, Color color) {
+
+    SDL_SetRenderDrawColor(render, color.r, color.g, color.b, 255);
+
     if (a.y > b.y) swap(a, b);
     if (a.y > c.y) swap(a, c);
-    if (b.y > c.y) swap(b, c);
-
-    //a
-    //b
-    //c
+    if (b.y > c.y) swap(b, c)
+    
+    int x1, x2;
 
     for(int y = a.y; y <= b.y; ++y) {
-        /*a -> b
-        a.y = k * a.x + b
-        b.y = k * b.x + b
+        if(a.x != b.x){
+            long double k = (long double)(a.y - b.y) / (a.x - b.x);
+            long double b1 = (long double)a.y - a.x * k;
+            if(k < 0.001 && k > -0.001)
+                break;
+            x1 = floor((y - b1) / k);
+        } else {
+            x1 = a.x;
+        }
 
-        a.y - b.y = k * (a.x - b.x)
-        */
+        if(a.x != c.x){
+            long double k1 = (long double)(a.y - c.y) / (a.x - c.x);
+            long double b2 = (long double)a.y - a.x * k1;
+            if(k1 < 0.001 && k1 > -0.001)
+                break;
+            x2 = ceil((y - b2) / k1);
+        } else {
+            x2 = a.x;
+        }
 
-        float k = (a.y - b.y) / (a.x - b.x);
-        float b = a.y - a.x * k;
+        if(x2 < x1)
+            swap(x1, x2);
 
-        /*a -> c
-        a.y = k1 * a.x + b1
-        c.y = k1 * c.x + b1
-        */
-
-        float k1 = (a.y - c.y) / (a.x - c.x);
-        float b1 = a.y - a.x * k1;
-
-        /*y = kx1 + b
-        y = k1x2 + b1
-
-        x1 = (y - b) / k
-        x2 = (y - b1) / k1
-        */
-
-        float x1 = (y - b) / k;
-        float x2 = (y - b1) / k1;
         for(int x = x1; x <= x2; ++x){
-            SDL_RenderDrawPoint(render, x, y);
+            if(x <= width && y <= height && x >=0 && y >= 0)
+                SDL_RenderDrawPoint(render, x, y);
+        }
+    }
+    
+    for(int y = b.y; y <= c.y; ++y) {
+        if(b.x != c.x){
+            long double k = (long double)(b.y - c.y) / (b.x - c.x);
+            long double b1 = (long double)b.y - b.x * k;
+            if(k < 0.001 && k > -0.001)
+                break;
+            x1 = floor((y - b1) / k);
+        } else {
+            x1 = b.x;
+        }
+
+        if(a.x != c.x){
+            long double k1 = (long double)(a.y - c.y) / (a.x - c.x);
+            long double b2 = (long double)a.y - a.x * k1;
+            if(k1 < 0.001 && k1 > -0.001)
+                break;
+            x2 = ceil((y - b2) / k1);
+        } else {
+            x2 = a.x;
+        }
+
+        if(x2 < x1)
+            swap(x1, x2);
+
+        for(int x = x1; x <= x2; ++x){
+            if(x <= width && y <= height && x >=0 && y >= 0)
+                SDL_RenderDrawPoint(render, x, y);
         }
     }
 }
@@ -162,8 +196,13 @@ int main() {
         draw_line(render, x0, y0, x1, y1);
     }*/
 
-    Vec2i a1 = {20, 20}, b1 = {100, 100}, c1 = {30, 80};
-    triangle(render, a1, b1, c1);
+    Vec2i a1 = {20, 20}, b1 = {120, 10}, c1 = {101, 30};
+    Color gray = {128, 128, 128};
+    triangle(render, a1, b1, c1, gray);
+    Vec2i a2 = {120, 120}, b2 = {180, 120}, c2 = {160, 290};
+    triangle(render, a2, b2, c2, gray);
+    Vec2i a3 = {1000, 20}, b3 = {1000, 120}, c3 = {800, 20};
+    triangle(render, a3, b3, c3, gray);
 
     SDL_RenderPresent(render);
     while (!quit) {
